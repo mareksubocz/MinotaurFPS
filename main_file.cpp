d80a5b08b9fd322a007adbaec868b619676ae84a
Unknown <mieszko335@gmail.com>
2019-07-02 14:10:50 +0200

    rename shader files

--- main_file.cpp
+++ main_file.cpp
@@ -80,7 +80,7 @@ int main( void )
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "vertex.glsl", "fragment.glsl" );
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
