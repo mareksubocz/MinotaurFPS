540fa4841e9efca128fadbbcd6a69122672ac3d8
Unknown <mieszko335@gmail.com>
2019-07-03 11:51:00 +0200

    renderowanie wielu obiektów

--- main_file.cpp
+++ main_file.cpp
@@ -16,10 +16,11 @@ GLFWwindow* window;
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-#include <shader.hpp>
-#include <texture.hpp>
-#include <controls.hpp>
-#include <objloader.hpp>
+#include "shader.hpp"
+#include "texture.hpp"
+#include "controls.hpp"
+#include "objloader.hpp"
+#include "drawmodel.hpp"
 
 #include <vector>
 
@@ -96,29 +97,39 @@ int main( void )
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-	// Load the texture
-	GLuint Texture = loadDDS("chair.dds");
 
-	// Get a handle for our "myTextureSampler" uniform
-	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
 
 	// Read our .obj file
-	std::vector<glm::vec3> vertices;
-	std::vector<glm::vec2> uvs;
-	std::vector<glm::vec3> normals; // Won't be used at the moment.
-	bool res = loadOBJ("chair.obj", vertices, uvs, normals);
+	std::vector<glm::vec3> gunVertices;
+	std::vector<glm::vec2> gunUvs;
+	std::vector<glm::vec3> gunNormals; // Won't be used at the moment.
+	loadOBJ("gun.obj", gunVertices, gunUvs, gunNormals);
 
-	// Load it into a VBO
+	// Read our .obj file
+	std::vector<glm::vec3> chairVertices;
+	std::vector<glm::vec2> chairUvs;
+	std::vector<glm::vec3> chairNormals; // Won't be used at the moment.
+	loadOBJ("chair.obj", chairVertices, chairUvs, chairNormals);
+
+	// Read our .obj file
+	std::vector<glm::vec3> wallVertices;
+	std::vector<glm::vec2> wallUvs;
+	std::vector<glm::vec3> wallNormals; // Won't be used at the moment.
+	loadOBJ("wall.obj", wallVertices, wallUvs, wallNormals);
+
+
+	GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
+	GLuint gunTexture = loadDDS("gun.dds");
+	GLuint wallTexture = loadDDS("wall.dds");
+	GLuint chairTexture = loadDDS("chair.dds");
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+
 
 	GLuint uvbuffer;
 	glGenBuffers(1, &uvbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+
 
 	do{
 
@@ -130,50 +141,39 @@ int main( void )
 
 		// Compute the MVP matrix from keyboard and mouse input
 		computeMatricesFromInputs();
-		glm::mat4 ProjectionMatrix = getProjectionMatrix();
-		glm::mat4 ViewMatrix = getViewMatrix();
-		glm::mat4 ModelMatrix = glm::mat4(1.0);
-		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
-		// Send our transformation to the currently bound shader,
-		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		glm::mat4 VP = getProjectionMatrix() * getViewMatrix();
 
-		// Bind our texture in Texture Unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
-		// Set our "myTextureSampler" sampler to use Texture Unit 0
-		glUniform1i(TextureID, 0);
 
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
 
-		// 2nd attribute buffer : UVs
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-		glVertexAttribPointer(
-			1,                                // attribute
-			2,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
-		);
 
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, vertices.size() );
 
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
+		glm::mat4 M = glm::mat4(1.0);
+
+        //==============================DRAW A GUN==========================================
+        //fill vertex buffer with vertex data
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, gunVertices.size() * sizeof(glm::vec3), &gunVertices[0], GL_STATIC_DRAW);
+
+        //same with UV buffer
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glBufferData(GL_ARRAY_BUFFER, gunUvs.size() * sizeof(glm::vec2), &gunUvs[0], GL_STATIC_DRAW);
+
+
+        drawModel(vertexbuffer, uvbuffer, M, VP, MatrixID, gunVertices.size(), gunTexture, TextureID);
+
+
+        //==============================DRAW A CHAIR==========================================
+        //fill vertex buffer with vertex data
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glBufferData(GL_ARRAY_BUFFER, chairVertices.size() * sizeof(glm::vec3), &chairVertices[0], GL_STATIC_DRAW);
+
+        //same with UV buffer
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glBufferData(GL_ARRAY_BUFFER, chairUvs.size() * sizeof(glm::vec2), &chairUvs[0], GL_STATIC_DRAW);
+
+        drawModel(vertexbuffer, uvbuffer, M, VP, MatrixID, chairVertices.size(), chairTexture, TextureID);
+
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -187,7 +187,9 @@ int main( void )
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
 	glDeleteProgram(programID);
-	glDeleteTextures(1, &Texture);
+	glDeleteTextures(1, &gunTexture);
+	glDeleteTextures(1, &wallTexture);
+	glDeleteTextures(1, &chairTexture);
 	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
